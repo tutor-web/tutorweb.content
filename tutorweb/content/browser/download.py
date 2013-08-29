9c76834f0f8bbc45ab28779243417864054bf094
Jamie Lentin <jm@lentin.co.uk>
2013-08-29 10:49:07 +0100

    Add image download for questions

--- tutorweb/content/browser/download.py
+++ tutorweb/content/browser/download.py
@@ -5,10 +5,10 @@ from plone.namedfile.utils import set_headers, stream_data
 from Products.Five.browser import BrowserView
 
 
-class DownloadPdfView(BrowserView):
+class DownloadView(BrowserView):
     """Stream back the contents of a NamedBlobFile field"""
     def __call__(self):
-        return self.downloadField('pdf')
+        raise NotImplemented
 
     def downloadField(self, fieldName):
         file = getattr(self.context, fieldName, None)
@@ -17,3 +17,15 @@ class DownloadPdfView(BrowserView):
 
         set_headers(file, self.request.response, filename=file.filename)
         return stream_data(file)
+
+
+class DownloadPdfView(DownloadView):
+    """Stream back the contents of the PDF field"""
+    def __call__(self):
+        return self.downloadField('pdf')
+
+
+class DownloadImageView(DownloadView):
+    """Stream back the contents of the Image field"""
+    def __call__(self):
+        return self.downloadField('image')
