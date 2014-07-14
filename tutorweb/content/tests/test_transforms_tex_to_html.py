fb2d5d89e48444597e444c2511eb8a978ebb8038
Jamie Lentin <jm@lentin.co.uk>
2014-07-14 17:39:41 +0100

    Use TtM to render TeX if available

--- tutorweb/content/tests/test_transforms_tex_to_html.py
+++ tutorweb/content/tests/test_transforms_tex_to_html.py
@@ -1,3 +1,6 @@
+import os.path
+import unittest
+
 from Products.CMFCore.utils import getToolByName
 
 from plone.app.testing import setRoles, login
@@ -5,12 +8,26 @@ from plone.app.testing import setRoles, login
 from .base import IntegrationTestCase
 from .base import MANAGER_ID, USER_A_ID, USER_B_ID, USER_C_ID
 
+import tutorweb.content.transforms.tex_to_html
+from tutorweb.content.transforms.tex_to_html import TTM_BINARY
+
+
+def expMathML(exp):
+    return """
+<math xmlns="http://www.w3.org/1998/Math/MathML">
+<mrow>
+<msup><mrow><mi>c</mi></mrow><mrow><mn>%s</mn></mrow>
+</msup>
+</mrow></math>""" % exp
+
 
 class ContentTypeTest(IntegrationTestCase):
 
-    def test_transformTeX(self):
-        """tex_to_html should have been registered and should transform content
+    def test_transformTeXNoTtM(self):
+        """tex_to_html should have been registered and should transform content,
+           even if TtM is missing
         """
+        tutorweb.content.transforms.tex_to_html.TTM_BINARY = '/tmp/non-existant'
         # Basically we just get a div around content
         self.assertEquals(
             self.doTransform("camel $c^2$"),
@@ -21,6 +38,35 @@ class ContentTypeTest(IntegrationTestCase):
         self.assertEquals(
             self.doTransform("camel $c^2$\ndromedary $c^1$"),
             """<div class="parse-as-tex">camel $c^2$<br />dromedary $c^1$</div>""",
+        )
+        tutorweb.content.transforms.tex_to_html.TTM_BINARY = TTM_BINARY
+
+    @unittest.skipUnless(os.path.isfile(TTM_BINARY), "Need TtM for this test")
+    def test_transformTeXTtM(self):
+        """tex_to_html should have been registered and should transform content
+        """
+        # Basically we just get a div around content
+        self.assertEquals(
+            self.doTransform("camel $c^2$"),
+            """<div class="ttm-output">camel %s</div>""" % expMathML(2),
+        )
+
+        # But as we use intelligent text, line breaks are also converted
+        self.assertEquals(
+            self.doTransform("camel $c^2$\ndromedary $c^1$"),
+            """<div class="ttm-output">camel %s\ndromedary %s</div>""" % (
+                expMathML(2),
+                expMathML(1),
+            ),
+        )
+
+        # We notice errors
+        self.assertEquals(
+            self.doTransform("\\begin{oatemise}And some other stuff"),
+            """<pre class="ttm-output error">HTML unicode style 1
+**** Unknown or ignored environment: \\begin{oatemise} Line 1
+Number of lines processed approximately 0</pre>
+<div class="ttm-output">And some other stuff</div>""",
         )
 
     def doTransform(self, content):
--- tutorweb/content/transforms/tex_to_html.py
+++ tutorweb/content/transforms/tex_to_html.py
@@ -1,8 +1,15 @@
+import cgi
+import os.path
+import subprocess
+
 from zope.interface import implements
 
 from plone.intelligenttext.transforms import convertWebIntelligentPlainTextToHtml
 
 from Products.PortalTransforms.interfaces import ITransform
+
+TTM_BINARY = '/usr/bin/ttm'
+
 
 class TexToHtml(object):
     """
@@ -27,10 +34,34 @@ class TexToHtml(object):
         raise AttributeError(attr)
 
     def convert(self, orig, data, **kwargs):
-        data.setData('<div class="parse-as-tex">' +
-            convertWebIntelligentPlainTextToHtml(orig) +
-            '</div>')
+        if os.path.isfile(TTM_BINARY):
+            p = subprocess.Popen(
+                [
+                    TTM_BINARY,
+                    '-a',   # Try to convert picture elements
+                    '-e3',  # inline epsfbox w/no icon
+                    '-r',   # Don't output a pre/postamble
+                    '-u',   # Unicode please
+                ],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            (out, err) = p.communicate(input=orig)
+            if '****' in err:
+                # Probably an error, show it.
+                data.setData('<pre class="ttm-output error">%s</pre>\n<div class="ttm-output">%s</div>' % (
+                    cgi.escape(err.strip()),
+                    out.strip(),
+                ))
+            else:
+                data.setData('<div class="ttm-output">%s</div>' % out.strip())
+        else:
+            data.setData('<div class="parse-as-tex">' +
+                         convertWebIntelligentPlainTextToHtml(orig) +
+                         '</div>')
         return data
+
 
 def register():
     return TexToHtml()
