abc5e67ba1c18588ad648357c9c982978063e9be
Jamie Lentin <jm@lentin.co.uk>
2014-07-25 08:22:19 +0100

    There's a theme package now, so do don't need this

--- setup.py
+++ setup.py
@@ -43,8 +43,6 @@ setup(
         'plone.app.transmogrifier',
         'transmogrify.dexterity',  
         'zope.app.component',
-        'collective.easyslider',   #make this part of the theme package
-
     ],
     extras_require={
         'test': [
