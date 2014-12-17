b7762d46f1a77df601a045819755a8d95a049095
Jamie Lentin <jm@lentin.co.uk>
2014-12-17 15:00:24 +0000

    Move all migration code together

--- tutorweb/content/browser/migrations.py
+++ tutorweb/content/browser/migrations.py
@@ -1,5 +1,6 @@
 import urlparse
 
+from collective.transmogrifier.interfaces import ITransmogrifier
 from tutorweb.content.transmogrifier.collectivejsonifysource import catalog_query, fetch_item, fetch_children
 
 from Products.CMFCore.utils import getToolByName
@@ -37,3 +38,19 @@ class LectureImportView(BrowserView):
 
         # Trigger slide import into our newly created lecture
         newLecture.unrestrictedTraverse('@@slide-copy')()
+
+
+class SlideImportView(BrowserView):
+    def __call__(self):
+        if 'src' not in self.request.form:
+            raise ValueError("Must provide a URL to a lecture in src")
+        transmogrifier = ITransmogrifier(self.context)
+        transmogrifier(
+            'tutorweb.content.transmogrifier.oldtutorwebslideimport',
+            definitions=dict(
+                url=self.request.form['src'],
+                folder='',
+                desturl=self.context.absolute_url(),
+            ),
+        )
+        return "success"
