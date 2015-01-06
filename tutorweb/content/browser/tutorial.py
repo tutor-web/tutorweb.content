99d6c41c92e446ce93f8e0a9bab1da0d2376b37b
Jamie Lentin <jm@lentin.co.uk>
2015-01-06 16:03:25 +0000

    Convert ~useless test view into an "update-pdf" view, to manually re-run pdflatex

--- tutorweb/content/browser/tutorial.py
+++ tutorweb/content/browser/tutorial.py
@@ -1,10 +1,26 @@
 from Products.Five.browser import BrowserView
+from plone.namedfile.file import NamedBlobFile
 
 from ..tex_generator import TexGenerator
 
-class TutorialTeXView(BrowserView):
-    """Convert tutorials into TeX"""
+class TutorialUpdatePDFView(BrowserView):
+    """Convert tutorials into PDF, update local copy"""
 
     def __call__(self):
-        tg = TexGenerator(self.context)
-        self.request.response.write(str(tg.outputFiles()))
+        try:
+            tg = TexGenerator(self.context)
+            self.request.response.write(tg.createPDF())
+            self.request.response.write("\n\nFinished\n")
+            lastOut = tg.outputFiles()[-1]
+            if lastOut.endswith('.pdf'):
+                with open(lastOut, 'r') as f:
+                    self.context.pdf = NamedBlobFile(
+                        data=f.read(),
+                        contentType=u'application/pdf',
+                        filename=u'',
+                    )
+                self.request.response.write("Tutorial PDF updated with %s\n" % lastOut)
+            else:
+                self.request.response.write("Tutorial PDF *NOT* updated\n")
+        finally:
+            tg.close()
