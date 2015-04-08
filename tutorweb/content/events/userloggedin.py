5c87c54270b27c778ce9307d0f212d93487440c2
Jamie Lentin <jm@lentin.co.uk>
2015-04-08 13:06:34 +0100

    Can get request directly

--- tutorweb/content/events/userloggedin.py
+++ tutorweb/content/events/userloggedin.py
@@ -1,21 +1,18 @@
-from zope.app.component.hooks import getSite
+from zope.globalrequest import getRequest
 
 
 def loggedInHandler(event):
     """If a user has logged in without having a full name yet, prompt 'em"""
     portal = getSite()
     user = event.object
-    request = getattr(portal, "REQUEST", None)
-    if not request:
-        # HTTP request is not present e.g.
-        # when doing unit testing / calling scripts from command line
-        return False
 
     # If user has a fullname and has accepted terms, carry on
     mb = portal.portal_membership.getAuthenticatedMember()
     if mb.getProperty('fullname') and mb.getProperty('accept', False):
-        return False
+        return
 
     # Otherwise, redirect to personal information
+    request = getRequest()
+    if request is None:
+        return
     request['next'] = portal.absolute_url() + "/@@personal-information"
-    return True
