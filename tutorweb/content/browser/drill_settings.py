4a954b07c6a364101b892fcdec4ea1d21b85db63
Jamie Lentin <jm@lentin.co.uk>
2016-09-22 15:51:58 +0100

    Make drill-settings :min/:max aware when overriding properties

--- tutorweb/content/browser/drill_settings.py
+++ tutorweb/content/browser/drill_settings.py
@@ -1,4 +1,5 @@
 import itertools
+import re
 
 from Acquisition import aq_parent
 from zope.component import queryUtility
@@ -16,12 +17,37 @@ class DrillSettingsView(JSONBrowserView):
                 return []
             return ((i['key'], i['value']) for i in settings)
 
+        def combine_settings(settings):
+            """Take iterator of single values, combine everything into a dict"""
+            out = dict()
+            for (k, v) in settings:
+                m = re.match("(.*):(min|max)$", k)
+
+                if m:
+                    # :min / :max property
+                    if not isinstance(out.get(m.group(1), None), dict):
+                        # Either property not set yet, or a non-dict value
+                        out[m.group(1)] = dict()
+                    out[m.group(1)][m.group(2)] = v
+                else:
+                    out[k] = v
+
+            # Moosh dicts back to :min and :max
+            for base_key in out.keys():
+                if not isinstance(out[base_key], dict):
+                    continue
+                for (k, v) in out[base_key].items():
+                    out['%s:%s' % (base_key, k)] = v
+                del out[base_key]
+
+            return out
+
         registry = queryUtility(IRegistry)
         if registry is None:
             raise ValueError("Cannot get Plone registry")
 
         # Combine, later items override previous ones
-        return dict(itertools.chain(
+        return combine_settings(itertools.chain(
             registry.get('tutorweb.content.lectureSettings', ()).items(),
             tlate(aq_parent(self.context).settings),
             tlate(self.context.settings),
---
+++ tutorweb/content/tests/test_browser_drill_settings.py
@@ -0,0 +1,70 @@
+from zope.component import queryUtility
+
+from plone.registry.interfaces import IRegistry
+
+from .base import IntegrationTestCase
+
+class DrillSettingsViewTest(IntegrationTestCase):
+    def get_settings(self, base_settings = None, tut_settings = None, lec_settings = None):
+        portal = self.layer['portal']
+
+        registry = queryUtility(IRegistry)
+        registry.records['tutorweb.content.lectureSettings'].value = base_settings
+        portal['dept1']['tut1'].settings = tut_settings
+        portal['dept1']['tut1']['lec1'].settings = lec_settings
+
+        return portal['dept1']['tut1']['lec1'].unrestrictedTraverse('@@drill-settings').asDict()
+
+    def test_drillsettings(self):
+        # lecture wins over tutorial, wins over registry
+        self.assertEqual(self.get_settings(
+            base_settings=dict(
+                base_only = u'0.1',
+                base_tut = u'0.01',
+                base_tut_lec = u'0.001',
+            ),
+            tut_settings=[
+                dict(key='base_tut', value=u'0.02'),
+                dict(key='base_tut_lec', value=u'0.002'),
+            ],
+            lec_settings=[
+                dict(key='base_tut_lec', value=u'0.003'),
+            ],
+        ), dict(
+            base_only = u'0.1',
+            base_tut = u'0.02',
+            base_tut_lec = u'0.003',
+        ))
+
+        # min/max properties override existing static values
+        self.assertEqual(self.get_settings(
+            base_settings=dict(
+                prop = u'0.5',
+            ),
+            tut_settings=[
+                dict(key='prop:min', value=u'0.1'),
+                dict(key='prop:max', value=u'1.0'),
+            ],
+            lec_settings=[
+                dict(key='prop:max', value=u'2.0'),
+            ],
+        ), {
+            'prop:min': u'0.1',
+            'prop:max': u'2.0',
+        })
+
+        # can override back to static value again
+        self.assertEqual(self.get_settings(
+            base_settings=dict(
+                prop = u'0.5',
+            ),
+            tut_settings=[
+                dict(key='prop:min', value=u'0.1'),
+                dict(key='prop:max', value=u'1.0'),
+            ],
+            lec_settings=[
+                dict(key='prop', value=u'8.0'),
+            ],
+        ), {
+            'prop': u'8.0',
+        })
