6b97abd65d8a923db275f2a39c13a939d3dd3910
Jamie Lentin <jm@lentin.co.uk>
2018-04-04 10:03:21 +0000

    download: Disable forcing download if there's a filename after the view

--- tutorweb/content/browser/download.py
+++ tutorweb/content/browser/download.py
@@ -1,6 +1,7 @@
 import mimetypes
 
-from zope.publisher.interfaces import NotFound
+from zope.interface import implements
+from zope.publisher.interfaces import IPublishTraverse, NotFound
 
 from plone.namedfile.utils import set_headers, stream_data
 
@@ -9,6 +10,22 @@ from Products.Five.browser import BrowserView
 
 class DownloadView(BrowserView):
     """Stream back the contents of a NamedBlobFile field"""
+    implements(IPublishTraverse)
+
+    def __init__(self, context, request):
+        super(DownloadView, self).__init__(context, request)
+        self.forceDownload = True
+
+    def publishTraverse(self, request, id):
+        # NB: We don't care what the filename is, just one is provided
+        # for the browser when saving
+        if self.forceDownload:
+            self.forceDownload = False
+        else:
+            # Only one level of traversal allowed
+            raise NotFound(self, id, request)
+        return self
+
     def __call__(self):
         raise NotImplemented
 
@@ -25,7 +42,7 @@ class DownloadView(BrowserView):
                 mimetypes.guess_extension(file.contentType) or '.dat',
             ])
 
-        set_headers(file, self.request.response, filename=fileName)
+        set_headers(file, self.request.response, filename=fileName if self.forceDownload else None)
         return stream_data(file)
 
 
