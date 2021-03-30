20fbf63b06d9297d7987b8f6257cb2b23af57614
Jamie Lentin <jm@lentin.co.uk>
2021-03-30 11:36:07 +0100

    jsonbrowserview: Add global pprint option, mostly for @@drill-settings

--- tutorweb/content/browser/jsonbrowserview.py
+++ tutorweb/content/browser/jsonbrowserview.py
@@ -14,6 +14,11 @@ class JSONBrowserView(BrowserView):
         """Return dict to be turned into JSON"""
         raise NotImplementedError
 
+    def json_format(self, o):
+        if self.request.form.get('pprint', None) is not None:
+            return json.dumps(o, indent=4)
+        return json.dumps(o)
+
     def __call__(self):
         try:
             # Is there a request body?
@@ -28,11 +33,11 @@ class JSONBrowserView(BrowserView):
             out = self.asDict(data)
             self.request.response.setStatus(200)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(out)
+            return self.json_format(out)
         except Unauthorized, ex:
             self.request.response.setStatus(403)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
+            return self.json_format(dict(
                 error=ex.__class__.__name__,
                 message=str(ex),
             ))
@@ -40,21 +45,21 @@ class JSONBrowserView(BrowserView):
             # Return as Unauthorized, so Javascript can redirect full page
             self.request.response.setStatus(403)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
+            return self.json_format(dict(
                 error=ex.__class__.__name__,
                 location=str(ex),
             ))
         except NotFound, ex:
             self.request.response.setStatus(404)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
+            return self.json_format(dict(
                 error=ex.__class__.__name__,
                 message=str(ex),
             ))
         except BadRequest, ex:
             self.request.response.setStatus(400)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
+            return self.json_format(dict(
                 error=ex.__class__.__name__,
                 message=str(ex),
             ))
@@ -65,7 +70,7 @@ class JSONBrowserView(BrowserView):
             logging.exception(ex)
             self.request.response.setStatus(500)
             self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
+            return self.json_format(dict(
                 error=ex.__class__.__name__,
                 message=str(ex),
                 stacktrace=traceback.format_exc() if DevelopmentMode else '',
